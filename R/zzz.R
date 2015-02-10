8e0d30367eb48f08fc71d8f96af260753bc07d26
Nathan Palmer <nathan.palmer@gmail.com>
2015-02-10 17:54:33 -0500

    updated JVM initialization to larger stack

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  .jinit(parameters=c("-Djsse.enableCBCProtection=false", "-Xmx1024m", "-d64"))
+  .jinit(parameters=c("-Djsse.enableCBCProtection=false", "-Xmx102400m", "-d64"))
   .jpackage(pkgname, lib.loc = libname)
 }
